53ecb396dc8f85d28fd95daf4b38f22b83f52534
CS252 <cs252@cs.purdue.edu>
2020-02-17 18:56:23 -0500

    START REPO SPRING2020

---
+++ command.hh
@@ -0,0 +1,25 @@
+#ifndef command_hh
+#define command_hh
+
+#include "simpleCommand.hh"
+
+// Command Data Structure
+
+struct Command {
+  std::vector<SimpleCommand *> _simpleCommandsArray;
+  std::string * _outFileName;
+  std::string * _inFileName;
+  std::string * _errFileName;
+  bool _backgnd;
+
+  Command();
+  void insertSimpleCommand( SimpleCommand * simpleCommand );
+
+  void clear();
+  void print();
+  void execute();
+
+  static SimpleCommand *_currSimpleCommand;
+};
+
+#endif
