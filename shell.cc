60b64ae7ef6e108d8320776cddf559a8a1bf78e4
huandrew99 <hu573@purdue.edu>
2020-03-01 17:55:06 -0500

    0 Mon Feb 17 09:54:55 EST 2020 0 Wed Feb 19 16:40:35 EST 2020 0 Sun Feb 23 16:26:54 EST 2020 0 Sun Feb 23 18:15:00 EST 2020 0 Sun Feb 23 23:25:04 EST 2020 0 Mon Feb 24 08:42:56 EST 2020 0 Mon Feb 24 08:43:18 EST 2020 1 Mon Feb 24 18:32:20 EST 2020 2 Mon Feb 24 21:55:37 EST 2020 2 Mon Feb 24 23:55:45 EST 2020 2 Mon Feb 24 23:56:03 EST 2020 2 Mon Feb 24 23:56:40 EST 2020 2 Wed Feb 26 00:07:12 EST 2020 2 Wed Feb 26 00:10:13 EST 2020 19 Wed Feb 26 00:22:56 EST 2020 20 Wed Feb 26 00:23:47 EST 2020 21 Wed Feb 26 00:56:38 EST 2020 20 Wed Feb 26 00:56:55 EST 2020 0 Wed Feb 26 00:58:04 EST 2020 21 Wed Feb 26 00:58:34 EST 2020 0 Sun Mar 1 15:42:18 EST 2020 20 Sun Mar 1 15:43:49 EST 2020 0 Sun Mar 1 15:44:19 EST 2020 0 Sun Mar 1 16:35:45 EST 2020 0 Sun Mar 1 16:36:22 EST 2020 0 Sun Mar 1 16:44:53 EST 2020 21 Sun Mar 1 16:45:30 EST 2020 0 Sun Mar 1 16:48:59 EST 2020 0 Sun Mar 1 17:01:14 EST 2020 21 Sun Mar 1 17:02:16 EST 2020 22 Sun Mar 1 17:42:53 EST 2020 22 Sun Mar 1 17:43:32 EST 2020 22 Sun Mar 1 17:45:58 EST 2020 22 Sun Mar 1 17:46:16 EST 2020

--- shell.cc
+++ shell.cc
@@ -9,14 +9,20 @@
 #include "shell.hh"
 
 extern "C" void disp( int sig ) {
-  fprintf( stderr, "\nsig:%d  Ouch!\n", sig);
+  //fprintf( stderr, "\nsig:%d  Ouch!\n", sig);
   if (sig == SIGINT) {
     printf("\n");
     //printf("myshell>");
     Shell::prompt();
-
   }
 
+}
+
+extern "C" void zombie( int sig) {
+  pid_t pid = wait3(0,0,NULL);
+  while (waitpid(-1, NULL, WNOHANG) > 0) {
+    printf("\n[%d] exited.", pid);
+  }
 }
 
 int yyparse(void);
@@ -37,6 +43,16 @@ int main() {
     exit(2);
   }
   
+  struct sigaction sa_zombie;
+  sa_zombie.sa_handler = zombie;
+  sigemptyset(&sa_zombie.sa_mask);
+  sa_zombie.sa_flags = SA_RESTART;
+  
+  if (sigaction(SIGCHLD, &sa_zomble, NULL)) {
+    perror("sigaction");
+    exit(2);
+  }
+
   if (isatty(0)) {
     Shell::prompt();
   }
