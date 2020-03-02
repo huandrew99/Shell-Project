bf403ec0df5b0b0f2834d53abed46b41cffcfa34
huandrew99 <hu573@purdue.edu>
2020-03-01 21:02:53 -0500

    0 Mon Feb 17 09:54:55 EST 2020 0 Wed Feb 19 16:40:35 EST 2020 0 Sun Feb 23 16:26:54 EST 2020 0 Sun Feb 23 18:15:00 EST 2020 0 Sun Feb 23 23:25:04 EST 2020 0 Mon Feb 24 08:42:56 EST 2020 0 Mon Feb 24 08:43:18 EST 2020 1 Mon Feb 24 18:32:20 EST 2020 2 Mon Feb 24 21:55:37 EST 2020 2 Mon Feb 24 23:55:45 EST 2020 2 Mon Feb 24 23:56:03 EST 2020 2 Mon Feb 24 23:56:40 EST 2020 2 Wed Feb 26 00:07:12 EST 2020 2 Wed Feb 26 00:10:13 EST 2020 19 Wed Feb 26 00:22:56 EST 2020 20 Wed Feb 26 00:23:47 EST 2020 21 Wed Feb 26 00:56:38 EST 2020 20 Wed Feb 26 00:56:55 EST 2020 0 Wed Feb 26 00:58:04 EST 2020 21 Wed Feb 26 00:58:34 EST 2020 0 Sun Mar 1 15:42:18 EST 2020 20 Sun Mar 1 15:43:49 EST 2020 0 Sun Mar 1 15:44:19 EST 2020 0 Sun Mar 1 16:35:45 EST 2020 0 Sun Mar 1 16:36:22 EST 2020 0 Sun Mar 1 16:44:53 EST 2020 21 Sun Mar 1 16:45:30 EST 2020 0 Sun Mar 1 16:48:59 EST 2020 0 Sun Mar 1 17:01:14 EST 2020 21 Sun Mar 1 17:02:16 EST 2020 22 Sun Mar 1 17:42:53 EST 2020 22 Sun Mar 1 17:43:32 EST 2020 22 Sun Mar 1 17:45:58 EST 2020 22 Sun Mar 1 17:46:16 EST 2020 2 Sun Mar 1 17:58:07 EST 2020 20 Sun Mar 1 18:02:25 EST 2020 23 Sun Mar 1 18:02:39 EST 2020 23 Sun Mar 1 18:05:42 EST 2020 23 Sun Mar 1 19:26:39 EST 2020 24.0 Sun Mar 1 19:34:03 EST 2020 3.0 Sun Mar 1 19:34:14 EST 2020 4 Sun Mar 1 19:38:01 EST 2020 22.0 Sun Mar 1 19:39:26 EST 2020 14.5 Sun Mar 1 19:47:18 EST 2020 24.0 Sun Mar 1 19:48:12 EST 2020 24.0 Sun Mar 1 19:55:32 EST 2020 24.0 Sun Mar 1 19:57:29 EST 2020 24.5 Sun Mar 1 20:48:29 EST 2020 25.0 Sun Mar 1 20:57:34 EST 2020

--- command.cc
+++ command.cc
@@ -199,6 +199,11 @@ void Command::execute() {
         return;
       }
 
+      if (strcmp(_simpleCommandsArray[j]->_argumentsArray[0]->c_str(), "cd") == 0) {
+        chdir(_simpleCommandsArray[j]->_argumentsArray[1]->c_str());
+
+      }
+
       if (j == simpleNum - 1) {
         if (_outFileName) {
           if (_append) {
