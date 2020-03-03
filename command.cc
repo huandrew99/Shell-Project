0b2e9954a848302f873dd38ff15e7c7feeddb3fa
huandrew99 <hu573@purdue.edu>
2020-03-02 22:04:56 -0500

    0 Mon Feb 17 09:54:55 EST 2020 0 Wed Feb 19 16:40:35 EST 2020 0 Sun Feb 23 16:26:54 EST 2020 0 Sun Feb 23 18:15:00 EST 2020 0 Sun Feb 23 23:25:04 EST 2020 0 Mon Feb 24 08:42:56 EST 2020 0 Mon Feb 24 08:43:18 EST 2020 1 Mon Feb 24 18:32:20 EST 2020 2 Mon Feb 24 21:55:37 EST 2020 2 Mon Feb 24 23:55:45 EST 2020 2 Mon Feb 24 23:56:03 EST 2020 2 Mon Feb 24 23:56:40 EST 2020 2 Wed Feb 26 00:07:12 EST 2020 2 Wed Feb 26 00:10:13 EST 2020 19 Wed Feb 26 00:22:56 EST 2020 20 Wed Feb 26 00:23:47 EST 2020 21 Wed Feb 26 00:56:38 EST 2020 20 Wed Feb 26 00:56:55 EST 2020 0 Wed Feb 26 00:58:04 EST 2020 21 Wed Feb 26 00:58:34 EST 2020 0 Sun Mar 1 15:42:18 EST 2020 20 Sun Mar 1 15:43:49 EST 2020 0 Sun Mar 1 15:44:19 EST 2020 0 Sun Mar 1 16:35:45 EST 2020 0 Sun Mar 1 16:36:22 EST 2020 0 Sun Mar 1 16:44:53 EST 2020 21 Sun Mar 1 16:45:30 EST 2020 0 Sun Mar 1 16:48:59 EST 2020 0 Sun Mar 1 17:01:14 EST 2020 21 Sun Mar 1 17:02:16 EST 2020 22 Sun Mar 1 17:42:53 EST 2020 22 Sun Mar 1 17:43:32 EST 2020 22 Sun Mar 1 17:45:58 EST 2020 22 Sun Mar 1 17:46:16 EST 2020 2 Sun Mar 1 17:58:07 EST 2020 20 Sun Mar 1 18:02:25 EST 2020 23 Sun Mar 1 18:02:39 EST 2020 23 Sun Mar 1 18:05:42 EST 2020 23 Sun Mar 1 19:26:39 EST 2020 24.0 Sun Mar 1 19:34:03 EST 2020 3.0 Sun Mar 1 19:34:14 EST 2020 4 Sun Mar 1 19:38:01 EST 2020 22.0 Sun Mar 1 19:39:26 EST 2020 14.5 Sun Mar 1 19:47:18 EST 2020 24.0 Sun Mar 1 19:48:12 EST 2020 24.0 Sun Mar 1 19:55:32 EST 2020 24.0 Sun Mar 1 19:57:29 EST 2020 24.5 Sun Mar 1 20:48:29 EST 2020 25.0 Sun Mar 1 20:57:34 EST 2020 25.0 Sun Mar 1 21:07:38 EST 2020 24.5 Sun Mar 1 21:13:02 EST 2020 4.0 Sun Mar 1 21:18:31 EST 2020 4.5 Sun Mar 1 21:35:08 EST 2020 26.0 Sun Mar 1 22:11:49 EST 2020 26.5 Sun Mar 1 22:30:29 EST 2020 5.5 Sun Mar 1 22:36:41 EST 2020 26.5 Sun Mar 1 23:25:20 EST 2020 26.5 Mon Mar 2 16:15:03 EST 2020 5.5 Mon Mar 2 16:20:59 EST 2020 24.5 Mon Mar 2 16:22:49 EST 2020 4.5 Mon Mar 2 16:24:42 EST 2020 5.5 Mon Mar 2 16:26:45 EST 2020 5.5 Mon Mar 2 16:57:22 EST 2020 26.0 Mon Mar 2 16:58:54 EST 2020 26.0 Mon Mar 2 16:59:55 EST 2020 26.5 Mon Mar 2 17:00:24 EST 2020 2.0 Mon Mar 2 18:12:51 EST 2020 5.0 Mon Mar 2 18:13:18 EST 2020 28.5 Mon Mar 2 18:13:53 EST 2020 7.5 Mon Mar 2 18:15:18 EST 2020 5.5 Mon Mar 2 18:17:37 EST 2020 7.5 Mon Mar 2 18:18:40 EST 2020 26.5 Mon Mar 2 18:30:40 EST 2020 28.5 Mon Mar 2 18:30:58 EST 2020 28.5 Mon Mar 2 18:45:22 EST 2020 28.5 Mon Mar 2 18:45:41 EST 2020 7.5 Mon Mar 2 18:47:05 EST 2020 7.5 Mon Mar 2 18:47:10 EST 2020 7.5 Mon Mar 2 18:47:14 EST 2020 7.5 Mon Mar 2 18:47:19 EST 2020 7.5 Mon Mar 2 18:47:23 EST 2020 7.5 Mon Mar 2 18:47:28 EST 2020 7.5 Mon Mar 2 18:47:32 EST 2020 2.0 Mon Mar 2 19:55:22 EST 2020 9.5 Mon Mar 2 19:55:50 EST 2020 9.5 Mon Mar 2 19:56:38 EST 2020 9.5 Mon Mar 2 19:56:43 EST 2020 9.5 Mon Mar 2 19:56:47 EST 2020 9.5 Mon Mar 2 19:56:52 EST 2020 9.5 Mon Mar 2 19:56:56 EST 2020 9.5 Mon Mar 2 19:57:01 EST 2020 9.5 Mon Mar 2 19:57:05 EST 2020 9.5 Mon Mar 2 20:05:48 EST 2020 9.5 Mon Mar 2 20:05:53 EST 2020 9.5 Mon Mar 2 20:05:57 EST 2020 9.5 Mon Mar 2 20:06:02 EST 2020 9.5 Mon Mar 2 20:06:06 EST 2020 9.5 Mon Mar 2 20:06:11 EST 2020 9.5 Mon Mar 2 20:06:15 EST 2020 9.5 Mon Mar 2 20:06:21 EST 2020 9.5 Mon Mar 2 20:06:33 EST 2020 9.5 Mon Mar 2 20:06:38 EST 2020 9.5 Mon Mar 2 20:06:42 EST 2020 9.5 Mon Mar 2 20:06:47 EST 2020 9.5 Mon Mar 2 20:06:51 EST 2020 9.5 Mon Mar 2 20:06:56 EST 2020 9.5 Mon Mar 2 20:07:00 EST 2020 9.5 Mon Mar 2 20:07:05 EST 2020 9.5 Mon Mar 2 20:07:09 EST 2020 9.5 Mon Mar 2 20:07:14 EST 2020 9.5 Mon Mar 2 20:07:18 EST 2020 9.5 Mon Mar 2 20:07:23 EST 2020 30.5 Mon Mar 2 20:07:50 EST 2020 30.5 Mon Mar 2 20:31:34 EST 2020 30.5 Mon Mar 2 21:22:17 EST 2020

--- command.cc
+++ command.cc
@@ -204,7 +204,7 @@ void Command::execute() {
         //printf("%s\n", _simpleCommandsArray[j]->_argumentsArray[1]->c_str());
         std::ifstream file(_simpleCommandsArray[j]->_argumentsArray[1]->c_str());
         std::string str;
-        std::getline(file,str)
+        std::getline(file,str);
           printf("%s\n", str.c_str());
          
         clear();
